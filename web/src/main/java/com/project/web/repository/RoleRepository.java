19bda207b13f6a326308737ba76e6c03960afee0
NojusKi <115144145+NojusKi@users.noreply.github.com>
2025-03-25 14:39:50 +0200

    Delete web directory

--- web/src/main/java/com/project/web/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.web.repository;
-
-import com.project.web.models.Role;
-import com.project.web.models.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
